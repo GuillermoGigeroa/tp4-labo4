ac5fdf539b4827264f78368bcd6e9efe5dff858e
guilleGigeroa <guillermo.gigeroa@hotmail.com>
2021-09-26 22:18:49 -0300

    Proyecto final - limpieza

--- src/appEscritorio_v1/Principal.java
+++
@@ -1,9 +0,0 @@
-package appEscritorio_v1;
-
-public class Principal {
-	public static void main(String[] args) {
-//		VentanaEjercicio1 ventana = new VentanaEjercicio1();
-//		ventana.setVisible(true);
-		System.out.println("Hola mundo!");
-	}
-}
--- src/appEscritorio_v3/Main.java
+++
@@ -1,8 +0,0 @@
-package appEscritorio_v3;
-
-public class Main {
-	public static void main(String[] args) {
-		VentanaPrincipal ventana = new VentanaPrincipal();
-		ventana.hacerVisible();
-	}
-}
